d6bddc4d05d82473dbd47d2372d9d8d4e81c7542
Himanshu402M <mauryahimanshu402@gmail.com>
2025-06-24 13:11:41 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20246767"))
 API_HASH = environ.get("API_HASH", "40c77323994b4c8b3dfc38273955ed3b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7831385577:AAGEHP8b9dRiF6NPWo-dEQsAeRBx11592kQ")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7995192897:AAF8OZwPl1mCC0GMILM0GGyJVeEtIzwCD8A")
 OWNER = int(environ.get("OWNER", "2001332759"))
 CREDIT = "〱⏤͟͞𝙃 𝙈🐦‍🔥 〄"
 AUTH_USER = os.environ.get('AUTH_USERS', '8034639315').split(',')
